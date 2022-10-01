90f6b65438d26a4fd8f80d9ec09d28efbe049eb4
nicomargo <nmargo@protonmail.com>
2022-10-01 04:17:39 -0300

    some test

--- script1.sh
+++ script1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-timedatectl set-ntp true
-pacstrap /mnt base linux linux-firmware grub dosfstools mtools vim os-prober efibootmgr polkit-gnome tar unrar zsh unzip base-devel neovim nano networkmanager wget thunar thunar-volman tmux thunar-archive-plugin flameshot gvfs gvfs-afc gvfs-smb git archlinux-wallpaper bspwm sxhkd rofi sddm sudo htop materia-gtk-theme alacritty xorg ranger picom intel-ucode ntfs-3g papirus-icon-theme gnome-disk-utility feh rofi linux-headers gthumb gufw fuse2 gtkmm pcsclite libcanberra ncurses firefox tumbler filezilla pulseaudio pavucontrol pulseaudio-alsa alsa-utils --noconfirm &&
+timedatectl set-ntp true &&
+pacstrap /mnt base linux linux-firmware grub dosfstools mtools vim os-prober efibootmgr zsh base-devel neovim nano networkmanager sudo htop intel-ucode --noconfirm &&
+#pacstrap /mnt base linux linux-firmware grub dosfstools mtools vim os-prober efibootmgr polkit-gnome tar unrar zsh unzip base-devel neovim nano networkmanager wget thunar thunar-volman tmux thunar-archive-plugin flameshot gvfs gvfs-afc gvfs-smb git archlinux-wallpaper bspwm sxhkd rofi sddm sudo htop materia-gtk-theme alacritty xorg ranger picom intel-ucode ntfs-3g papirus-icon-theme gnome-disk-utility feh rofi linux-headers gthumb gufw fuse2 gtkmm pcsclite libcanberra ncurses firefox tumbler filezilla pulseaudio pavucontrol pulseaudio-alsa alsa-utils --noconfirm &&
 genfstab -U /mnt >> /mnt/etc/fstab &&
 arch-chroot /mnt ./script2.sh
--- script2.sh
+++ script2.sh
@@ -2,7 +2,6 @@
 ln -sf /usr/share/zoneinfo/America/Buenos_Aires /etc/localtime &&
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 sed -i 's/#%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/locale.gen
-
 locale-gen
 hwclock --systohc &&
 pacman-optimize &&
