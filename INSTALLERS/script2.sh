95f6f6180d093d60beb31bc55a3891aea0ca5e19
nicomargo <nmargo@protonmail.com>
2022-10-01 06:38:54 -0300

    a lot of changes

--- INSTALLERS/script2.sh
+++ INSTALLERS/script2.sh
@@ -7,9 +7,9 @@ sed -i 37c"ParallelDownloads = 5" /etc/pacman.conf &&
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
 locale-gen
 hwclock --systohc &&
-#echo "root:${rootpass}" | chpasswd
+echo "root:${rootpass}" | chpasswd
 useradd -m ${username} -G wheel &&
-#echo "$username:$userpass" | chpasswd &&
+echo "$username:$userpass" | chpasswd &&
 git clone https://github.com/NvChad/NvChad /home/${username}/.config/nvim --depth 1 &&
 chsh -s /bin/zsh root &&
 chsh -s /bin/zsh ${username} &&
@@ -17,7 +17,7 @@ systemctl enable sddm NetworkManager &&
 mkdir /boot/EFI &&
 #reflector -a 30 -f 7 -l 15 --sort rate --save /etc/pacman.d/mirrorlist
 sed -i 37c"ParallelDownloads = 5" /etc/pacman.conf &&
-sed -i 's/^#[multilib]/[multilib]/' /etc/pacman.conf
-sed -i 's/^#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/' /etc/pacman.conf
+#sed -i 's/^#[multilib]/[multilib]/' /etc/pacman.conf
+#sed -i 's/^#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/' /etc/pacman.conf
 exit
 arch-chroot -u ${username}:${username} /mnt ./INSTALLERS/script3.sh
--- INSTALLERS/script3.sh
+++ INSTALLERS/script3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /opt &&
-git clone https://aur.archlinux.org/yay.git &&
-cd yay &&
-makepkg -si &&
-#yay -S wps-office visual-studio-code-bin brave-bin vmware-workstation --noconfirm &&
+cd ~/ &&
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin &&
+makepkg -si --noconfirm &&
+yay -S wps-office visual-studio-code-bin brave-bin vmware-workstation --noconfirm &&
 echo "Install grub, see readme"
