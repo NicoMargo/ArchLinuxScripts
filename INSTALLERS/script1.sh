544a4f6a9cffec1915ea0eb0cff91714c8dc4af2
nicomargo <nmargo@protonmail.com>
2022-10-02 05:50:17 -0300

    test

--- INSTALLERS/script1.sh
+++ INSTALLERS/script1.sh
@@ -28,14 +28,19 @@ timedatectl set-ntp true &&
 pacman -Sy archlinux-keyring reflector --noconfirm &&
 sed -i 37c"ParallelDownloads = 6" /etc/pacman.conf &&
 #reflector -a 30 -f 7 -l 15 --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware grub dosfstools vim efibootmgr zsh base-devel sddm plasma git neovim nano networkmanager sudo htop intel-ucode --noconfirm &&
+pacstrap /mnt base linux linux-firmware grub dosfstools vim unzip efibootmgr zsh base-devel sddm plasma git neovim nano archlinux-wallpaper networkmanager sudo htop intel-ucode --noconfirm &&
 #pacstrap /mnt base linux linux-firmware grub dosfstools mtools vim os-prober efibootmgr polkit-gnome tar unrar zsh unzip base-devel neovim nano networkmanager wget thunar thunar-volman tmux thunar-archive-plugin flameshot gvfs gvfs-afc gvfs-smb git archlinux-wallpaper bspwm sxhkd rofi sddm sudo htop materia-gtk-theme alacritty xorg ranger picom intel-ucode ntfs-3g papirus-icon-theme gnome-disk-utility feh rofi linux-headers gthumb gufw fuse2 gtkmm pcsclite libcanberra ncurses firefox tumbler filezilla pulseaudio pavucontrol pulseaudio-alsa alsa-utils --noconfirm &&
 genfstab -U /mnt >> /mnt/etc/fstab &&
 cp config.conf INSTALLERS
 cp -R INSTALLERS /mnt
+git clone https://github.com/NicoMargo/MyArchLinuxConfig /INSTALLERS
+cp -R /INSTALLERS/MyArchLinuxConfig /home/${username}/.config
 arch-chroot /mnt ./INSTALLERS/script2.sh
 arch-chroot -u ${username}:${username} /mnt /home/${username}/INSTALLERS/script3.sh
+wget -P /usr/share/fonts/TTF https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Terminus.zip
+unzip /usr/share/fonts/TTF/Terminus.zip
+#Cleaning
 chmod -R 731 /mnt/root
-
 sed -i 's/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /mnt/etc/sudoers
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /mnt/etc/sudoers
+rm -R /mnt/INSTALLERS
