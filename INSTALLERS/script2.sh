0a611c148c121b2fcc0c1fc305c2f11982518d0f
nicomargo <nmargo@protonmail.com>
2022-10-02 07:14:19 -0300

    test

--- INSTALLERS/script2.sh
+++ INSTALLERS/script2.sh
@@ -19,10 +19,10 @@ mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=Arch4 &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
 wget -P /usr/share/fonts/TTF https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Terminus.zip
-unzip /usr/share/fonts/TTF/Terminus.zip /usr/share/fonts/TTF
-
-git clone https://github.com/NicoMargo/MyArchLinuxConfig /INSTALLERS
-cp -R /INSTALLERS/MyArchLinuxConfig /home/${username}/.config
+unzip /usr/share/fonts/TTF/Terminus.zip /usr/share/fonts/TTF 
+mkdir /INSTALLERS/dotfiles
+git clone https://github.com/NicoMargo/MyArchLinuxConfig /INSTALLERS/dotfiles
+mv -f /INSTALLERS/dotfiles/* /home/${username}/.config
 
 #reflector -a 30 -f 7 -l 15 --sort rate --save /etc/pacman.d/mirrorlist
 sed -i 37c"ParallelDownloads = 5" /etc/pacman.conf &&
