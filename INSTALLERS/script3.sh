b236c81b869d90386d3ffe7fe48a7d93d4f12381
nicomargo <nmargo@protonmail.com>
2022-10-04 00:04:23 -0300

    font

--- INSTALLERS/script3.sh
+++ INSTALLERS/script3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source ./INSTALLERS/config.conf
 cd /home/${username} &&
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin &&
-makepkg -si --noconfirm &&
+#git clone https://aur.archlinux.org/yay-bin.git
+#cd yay-bin &&
+#makepkg -si --noconfirm &&
 #yay -S wps-office visual-studio-code-bin brave-bin vmware-workstation --noconfirm &&
-yay -S brave-bin --noconfirm 
+#yay -S brave-bin --noconfirm 
 
 #-------------DOT FILES---------------cd
 mkdir /home/${username}/INSTALLERS/dotfiles
