4307587307d83bb12b54f7a036eb026499596874
nicomargo <nmargo@protonmail.com>
2022-10-04 00:03:05 -0300

    font

--- INSTALLERS/script3.sh
+++ INSTALLERS/script3.sh
@@ -7,10 +7,10 @@ makepkg -si --noconfirm &&
 #yay -S wps-office visual-studio-code-bin brave-bin vmware-workstation --noconfirm &&
 yay -S brave-bin --noconfirm 
 
-#-------------DOT FILES---------------
+#-------------DOT FILES---------------cd
 mkdir /home/${username}/INSTALLERS/dotfiles
 git clone https://github.com/NicoMargo/MyArchLinuxConfig /home/${username}/INSTALLERS/dotfiles
-mv -f /home/${username}/INSTALLERS/dotfiles/* /home/${username}/.config
+mv -f /home/${username}/INSTALLERS/dotfiles/* /home/${username}/.config/
 git clone https://github.com/NvChad/NvChad /home/${username}/.config/nvim --depth 1 &&
 
 
